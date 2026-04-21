4bd0accc11958d929d059ece26792abe1e8de608
xiaokaihan <2316357233@qq.com>
2026-04-22 00:09:21 +0800

    fix: 修复 keystatic route handler 导入错误

--- app/api/keystatic/[...params]/route.ts
+++ app/api/keystatic/[...params]/route.ts
@@ -1,3 +1,8 @@
-import { handler } from '@keystatic/next/route-handler';
+import { makeRouteHandler } from '@keystatic/next/route-handler';
+import config from '../../../../keystatic.config';
+
+const handler = makeRouteHandler({
+  config: config,
+});
 
 export { handler as GET, handler as POST };
